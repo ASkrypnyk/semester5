24151a75a60aa6ca6e16f8dccbeb55a9c4a7af24
Andrii Skripnik <andryskypnyk@gmail.com>
2020-09-20 00:09:48 +0300

    completed lab

--- SP/lab1/src/com/company/Main.java
+++ SP/lab1/src/com/company/Main.java
@@ -1,23 +1,33 @@
 package com.company;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.io.*;
 
-public class Main {
 
+public class Main {
     public static void main(String[] args) {
 
-                try(FileInputStream fin=new FileInputStream("C://Users//Skipnik//Documents//GitHub//semester5//SP//lab1//java.txt"))
-                {
-                    System.out.printf("File size: %d bytes \n", fin.available());
-
-                    int i=-1;
-                    while((i=fin.read())!=-1){
-
-                        System.out.print((char)i);
-                    }
-                }
-                catch(IOException ex){
-
-                    System.out.println(ex.getMessage());
-                }
-            }
+        String text = null;
+        try {
+            text = new String(Files.readAllBytes(Paths.get("text.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        List<String> list = Arrays.asList(text.toLowerCase().replaceAll("[-.?!)(,:;'–\n]", "").split(" "));
+
+        Set<String> uniqueWords = new HashSet<String>(list);
+        for (String word : uniqueWords) {
+            if (word.length() > 30) {
+                System.out.println(word.substring(0, 30) + ": " + Collections.frequency(list, word));
+            } else
+                System.out.println(word + ": " + Collections.frequency(list, word));
+        }
+        }
+    }
+
